f7429bfe9bad9809f8eae166125e2080e728636e
Mahdi Fazli <mahdifazli5459@gmail.com>
2023-12-02 21:02:40 +0330

    added the menu layout and some assets in the assets folder

--- maharat/src/App.js
+++ maharat/src/App.js
@@ -1,14 +1,14 @@
-import './App.scss'
 import { Routes, Route } from 'react-router-dom'
-import Layout from './components/Layout'
+import Layout from './components/Layout/index.js'
+import './App.scss'
+import Home from './components/Home/index.js'
 
 const App = () => {
   return (
-    <>
     <Routes>
       <Route path='/' element={<Layout />} />
+      <Route index element={<Home />} />
     </Routes>
-    </>
   )
 }
 
---
+++ maharat/src/components/Home/index.js
@@ -0,0 +1,11 @@
+import './index.scss'
+
+const Home = () => {
+  return (
+    <div>
+      hello
+    </div>
+  )
+}
+
+export default Home;
