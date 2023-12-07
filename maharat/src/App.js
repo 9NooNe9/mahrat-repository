87b183e2da79009d56963b25c6b19a0e7361fc96
Mahdi Fazli <mahdifazli5459@gmail.com>
2023-12-07 15:42:22 +0330

    idk

--- maharat/src/App.js
+++ maharat/src/App.js
@@ -1,15 +1,16 @@
-import { Routes, Route } from 'react-router-dom'
-import Layout from './components/Layout/index.js'
-import './App.scss'
-import Home from './components/Home/index.js'
+import { Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout/index.js";
+import "./App.scss";
+import Home from "./components/Home/index.js";
 
 const App = () => {
   return (
     <Routes>
-      <Route path='/' element={<Layout />} />
-      <Route index element={<Home />} />
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+      </Route>
     </Routes>
-  )
-}
+  );
+};
 
 export default App;
--- maharat/src/components/Layout/index.js
+++ maharat/src/components/Layout/index.js
@@ -1,27 +1,24 @@
-import { Outlet } from 'react-router-dom';
-import Sidebar from '../Sidebar';
-import '../../App.scss'
-import './index.scss'
+import { Outlet } from "react-router-dom";
+import Sidebar from "../Sidebar";
+import "./index.scss";
 
 const Layout = () => {
   return (
-    <div className='App'>
+    <div className="App">
       <Sidebar />
-      <div className='page'>
-        <span className='tags top-tags'>
-          &lt;body&gt;
-        </span>
+      <div className="page">
+        <span className="tags top-tags">&lt;body&gt;</span>
 
         <Outlet />
 
-        <span className='tags bottom-tags'>
+        <span className="tags bottom-tags">
           &lt;/body&gt;
           <br />
-          <span className='bottom-tag-html'>&lt;/html&gt;</span>
-          </span>
+          <span className="bottom-tag-html">&lt;/html&gt;</span>
+        </span>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Layout;
