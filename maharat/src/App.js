2ea56a7b14198f0655c9c52aacf87961cf3281a7
Mahdi Fazli <mahdifazli5459@gmail.com>
2023-12-11 15:14:03 +0330

    20 azar working at UI

--- maharat/src/App.js
+++ maharat/src/App.js
@@ -2,12 +2,14 @@ import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout/index.js";
 import "./App.scss";
 import Home from "./components/Home/index.js";
+import About from "./components/About/index.js";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
       </Route>
     </Routes>
   );
--- maharat/src/components/Home/Logo/index.js
+++ maharat/src/components/Home/Logo/index.js
@@ -1,11 +1,40 @@
 import "./index.scss";
 import LogoS from "../../../assets/images/logo-s.png";
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
+import gsap from "gsap-trial";
+import DrawSVGPlugin from "gsap-trial/DrawSVGPlugin";
 
 const Logo = () => {
   const bgRef = useRef();
   const outlineLogoRef = useRef();
   const solidLogoRef = useRef();
+
+  useEffect(() => {
+    gsap.registerPlugin(DrawSVGPlugin);
+
+    gsap
+      .timeline()
+      .to(bgRef.current, {
+        duration: 1,
+        opacity: 1,
+      })
+      .from(outlineLogoRef.current, {
+        drawSVG: 0,
+        duration: 20,
+      });
+
+    gsap.fromTo(
+      solidLogoRef.current,
+      {
+        opacity: 0,
+      },
+      {
+        opacity: 0,
+        delay: 4,
+        duration: 4,
+      }
+    );
+  }, []);
 
   return (
     <div className="logo-container" ref={bgRef}>
